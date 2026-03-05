7c79580277373fd94c035d1e648fff29791abf36
PasGat <47112536+PasGat@users.noreply.github.com>
2026-03-05 14:38:41 +0000

    [PG][OPS-14547] Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.6.0"
+  val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"    %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"    %% "play-frontend-hmrc-play-30"            % "12.31.0",
+    "uk.gov.hmrc"    %% "play-frontend-hmrc-play-30"            % "12.32.0",
     "org.typelevel"  %% "cats-core"                             % "2.13.0",
-    "uk.gov.hmrc"    %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"    %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "com.beachape"   %% "enumeratum-play"                       % "1.9.5",
     "io.lemonlabs"   %% "scala-uri"                             % "4.0.3"
   // format: ON
